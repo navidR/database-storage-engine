a0e039136b89bec207a9abda69e2d8ecd941bfae
Navid Rahimi <navidr@sfu.ca>
2019-01-26 19:22:30 -0800

    Init libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include <benchmark/benchmark.h>
+#include <tsl/hopscotch_map.h>
 
 using namespace std;
 
-int main()
+
+int main(int argc, char **argv)
 {
-  cout << "Hello World!" << endl;
+  google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "Glog";
+
+  tsl::hopscotch_map<int, int> map = {{1, 1}, {2, 1}, {3, 1}};
+  for(auto it = map.begin(); it != map.end(); ++it) {
+      //it->second = 2; // Illegal
+      it.value() = 2; // Ok
+  }
+
+
+
   return 0;
 }
