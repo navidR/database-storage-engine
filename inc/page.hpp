58fa6305ef60063b5664bf0ad9765f49e3c28764
Navid Rahimi <navidr@sfu.ca>
2019-01-27 11:18:51 -0800

    Adding interface

--- inc/page.hpp
+++ inc/page.hpp
@@ -10,6 +10,21 @@ public:
     void serialize(void *) const;
     void deserialize(void *);
 
+    /* Create a page with specified page size and record size */
+    static Page *CreatePage(uint32_t page_size, uint32_t record_size);
+
+    /* Insert a record. Returns true if the insertion was successful, false
+     * otherwise. There is no need to check for whether the record already
+     * exists in the page (i.e., duplication is allowed).
+     */
+    bool Insert(const char *record);
+
+    /* Read a record with a given RID. Returns a pointer to the record,
+     * NULL if it does not exist. Note that the higher order bits in rid refer
+     * to the page ID, and the lower 32 bits refer to the slot ID.
+     */
+    const char *Read(uint64_t rid);
+
     PageHeader *pageHeader;
 
 private:
--- src/page.cpp
+++ src/page.cpp
@@ -15,3 +15,19 @@ void Page::deserialize(void *ptr)
 {
 
 }
+
+Page* Page::CreatePage(uint32_t page_size, uint32_t record_size)
+{
+
+}
+
+
+bool Page::Insert(const char *record)
+{
+
+}
+
+const char* Page::Read(uint64_t rid)
+{
+
+}
