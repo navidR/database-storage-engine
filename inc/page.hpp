e3e03e54e9ba457b0dcafd894aa83d7f07a20ab8
Navid Rahimi <navidr@sfu.ca>
2019-01-28 08:57:20 -0800

    adding table

--- inc/page.hpp
+++ inc/page.hpp
@@ -9,27 +9,16 @@ public:
     Page(uint32_t);
     void serialize(void *) const;
     void deserialize(void *);
-
-    /* Create a page with specified page size and record size */
-    static Page *CreatePage(uint32_t page_size, uint32_t record_size);
-
-    /* Insert a record. Returns true if the insertion was successful, false
-     * otherwise. There is no need to check for whether the record already
-     * exists in the page (i.e., duplication is allowed).
-     */
     bool Insert(const char *record);
+    const char* Read(uint64_t rid);
+//    bool writeToFile();
 
-    /* Read a record with a given RID. Returns a pointer to the record,
-     * NULL if it does not exist. Note that the higher order bits in rid refer
-     * to the page ID, and the lower 32 bits refer to the slot ID.
-     */
-    const char *Read(uint64_t rid);
+    static Page *CreatePage(uint32_t page_size, uint32_t record_size);
 
     PageHeader *pageHeader;
 
 private:
     uint32_t page_identifier;
-
 };
 
 #endif // PAGE_HPP
